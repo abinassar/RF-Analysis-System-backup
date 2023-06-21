b968fc8eb0a2fb511c5dcc5f02d0104216302424
abinassar <abinassar@gmail.com>
2023-06-21 19:13:47 -0400

    refactor: improvements in show compass in the map page

--- src/app/pages/home/pages/map/map.page.ts
+++ src/app/pages/home/pages/map/map.page.ts
@@ -8,6 +8,12 @@ import { GeoPoint } from '../../../../shared/models/geographic';
 // import "leaflet-control-geocoder/dist/Control.Geocoder.scss";
 import "leaflet-control-geocoder/dist/Control.Geocoder.js";
 
+// Compass imports
+
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
+import { DeviceOrientation, DeviceOrientationCompassHeading } from '@ionic-native/device-orientation/ngx';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
+
 const iconRetinaUrl = 'assets/marker-icon-2x.png';
 const iconUrl = 'assets/marker-icon.png';
 const shadowUrl = 'assets/marker-shadow.png';
@@ -39,10 +45,50 @@ export class MapPage {
 
   createMaker: boolean = false;
 
+  // Compass variables
+
+  // We will call this variable on home.page.html file
+  data: DeviceOrientationCompassHeading;
+  public currentLocation: any = null;
+
+  // Initial Kaaba location that we've got from google maps
+  private kaabaLocation: {lat:number,lng:number} = {lat: 21.42276, lng: 39.8256687};
+
+  // Initial Qibla Location
+  public qiblaLocation = 0;
+
   constructor(private actionSheetController: ActionSheetController,
               private menu: MenuController,
               private linkModalCtrl: ModalController,
-              private settingsService: SettingsService) {}
+              private settingsService: SettingsService,
+              private deviceOrientation: DeviceOrientation,
+              private geolocation: Geolocation) {
+
+
+    // Watch the device compass heading change
+    this.deviceOrientation.watchHeading().subscribe((res: DeviceOrientationCompassHeading) => {
+      this.data = res;
+      // Change qiblaLocation when currentLocation is not empty 
+      if (!!this.currentLocation) {
+        const currentQibla = res.magneticHeading - this.getQiblaPosition();
+        this.qiblaLocation = currentQibla > 360 ? currentQibla%360 : currentQibla;
+      }
+      }
+    );
+
+    // Watch the device compass heading change
+    this.deviceOrientation.watchHeading().subscribe((res: DeviceOrientationCompassHeading) => {
+      this.data = res;
+    });
+
+    // Watch current location
+    this.geolocation.watchPosition().subscribe((res) => {
+        this.currentLocation = res;
+    });
+
+
+
+  }
 
   ngOnInit() {
 
@@ -211,6 +257,40 @@ export class MapPage {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
     //Add 'implements AfterViewInit' to the class.
     
+  }
+
+  // Compass logic
+
+  getQiblaPosition() {
+    // Convert all geopoint degree to radian before jump to furmula
+    const currentLocationLat = this.degreeToRadian(this.currentLocation.coords.latitude);
+    const currentLocationLng = this.degreeToRadian(this.currentLocation.coords.longitude);
+    const kaabaLocationLat = this.degreeToRadian(this.kaabaLocation.lat);
+    const kaabaLocationLng = this.degreeToRadian(this.kaabaLocation.lng);
+
+    // Use Basic Spherical Trigonometric Formula
+    return this.radianToDegree(
+      Math.atan2(
+        Math.sin(kaabaLocationLng-currentLocationLng),
+        (Math.cos(currentLocationLat) * Math.tan(kaabaLocationLat) - Math.sin(currentLocationLat) * Math.cos(kaabaLocationLng - currentLocationLng))
+      )
+    );
+  }
+
+  /**
+   * Convert from Radian to Degree
+   * @param radian 
+   */
+  radianToDegree(radian: number) {
+    return radian * 180 / Math.PI;
+  }
+
+  /**
+   * Convert from Degree to Radian
+   * @param degree 
+   */
+  degreeToRadian(degree: number) {
+    return degree * Math.PI / 180;
   }
 
 }
